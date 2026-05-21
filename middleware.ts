729c751f82247c1416f3dbce91d847092b6877ba
YakerHuertas26 <huertas2602@gmail.com>
2026-05-20 23:12:02 -0500

    [feat]: mofication structure for login

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { jwtVerify } from 'jose'
-
-import {
-    jwtPayload,
-    RoleName
-} from '@/src/feature/auth/types/auth.types'
+import {jwtPayload,RoleName} from '@/src/feature/auth/types/auth.types'
 
 const jwtSecret = process.env.JWT_SECRET
 
@@ -12,8 +8,7 @@ if (!jwtSecret) {
     throw new Error('JWT_SECRET is not defined')
 }
 
-const SECRET =
-    new TextEncoder().encode(jwtSecret)
+const SECRET = new TextEncoder().encode(jwtSecret)
 
 const ROLE_ROUTES: Record<RoleName, string[]> = {
     Admin: ['/admin/dashboard'],
@@ -21,78 +16,48 @@ const ROLE_ROUTES: Record<RoleName, string[]> = {
     Vendedor: ['/vendedor/dashboard'],
 }
 
-export async function middleware(
-    request: NextRequest
-) {
-
+export async function middleware( request: NextRequest) {
+    // Obtenemos la ruta a la que se intenta acceder
     const { pathname } = request.nextUrl
 
-    // =========================
-    // COOKIE
-    // =========================
+    // COOKIE: obtenemos el token de la cookie (si existe)
+    const token = request.cookies.get('auth_token')?.value
 
-    const token =
-        request.cookies.get('auth_token')?.value
+    // ** 2 casos de verificación de token: LOGIN y RUTAS PROTEGIDAS **
 
-    // =========================
-    // LOGIN
-    // =========================
-
+// ==LOGIN: si es la ruta de login, solo la mostramos a usuarios NO autenticados
     if (pathname.startsWith('/login')) {
-
-        // no token → mostrar login
+        // cuando no hay token → mostrar login
         if (!token) {
             return NextResponse.next()
         }
-
+        // Cundo ya hay un token -> verifica y obtengo el payload con el rol y direcciono
         try {
-
-            const { payload } =
-                await jwtVerify<jwtPayload>(
-                    token,
-                    SECRET
-                )
-
+            const { payload } = await jwtVerify<jwtPayload>(token,SECRET)
             const roleName = payload.roleName
+            const redirectTo =ROLE_ROUTES[roleName][0]
 
-            const redirectTo =
-                ROLE_ROUTES[roleName]?.[0]
-                ?? '/dashboard'
-
-            // ya autenticado → dashboard
-            return NextResponse.redirect(
-                new URL(redirectTo, request.url)
-            )
+            // ya autenticado redirreciona al dashboard
+            return NextResponse.redirect(new URL(redirectTo, request.url))
 
         } catch {
-
             // token inválido → mostrar login
             return NextResponse.next()
         }
     }
 
-    // =========================
-    // RUTAS PROTEGIDAS
-    // =========================
+// ===RUTAS PROTEGIDAS
 
-    // sin token → login
+    // sin token → lo devuelve al login
     if (!token) {
-
-        return NextResponse.redirect(
-            new URL('/login', request.url)
-        )
+        return NextResponse.redirect(new URL('/login', request.url))
     }
 
+    // Con token → verifica si es válido y si el rol tiene acceso a la ruta
     try {
-
-        const { payload } =
-            await jwtVerify<jwtPayload>(
-                token,
-                SECRET
-            )
-
-        const allowed =
-            ROLE_ROUTES[payload.roleName] ?? []
+        const { payload } =await jwtVerify<jwtPayload>(token,SECRET)
+        // obtengo las ruta permitidas para el rol del usuario
+        const allowed = ROLE_ROUTES[payload.roleName] ?? []
 
         const ok = allowed.some((route) =>
             pathname.startsWith(route)
@@ -100,26 +65,20 @@ export async function middleware(
 
         // rol sin acceso
         if (!ok) {
-
             return NextResponse.redirect(
-                new URL(
-                    allowed[0] ?? '/login',
-                    request.url
-                )
+                new URL(allowed[0] ?? '/login', request.url)
             )
         }
 
         return NextResponse.next()
 
     } catch {
-
         // token expirado/inválido
         const res = NextResponse.redirect(
             new URL('/login', request.url)
         )
 
         res.cookies.delete('auth_token')
-
         return res
     }
 }
