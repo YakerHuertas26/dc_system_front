833cbd6d0a5b60334d1a175310ecfeada9c06e00
YakerHuertas26 <huertas2602.com>
2026-08-17 10:50:56 -0500

    [Feat]: add component modal

--- src/shared/components/shadcn/components.tsx
+++ src/shared/components/shadcn/components.tsx
@@ -1,6 +1,9 @@
 import * as React from "react"
 import { Avatar as AvatarPrimitive } from "@base-ui/react/avatar"
 import { LoaderIcon } from "lucide-react"
+import { AlertDialog as AlertDialogPrimitive } from "@base-ui/react/alert-dialog"
+import { Button as ButtonPrimitive } from "@base-ui/react/button"
+import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/src/shared/utils/cn.utils"
 
@@ -153,19 +156,235 @@ function TableCaption({
     )
 }
 
+// BUTTON 
+const buttonVariants = cva(
+    "group/button inline-flex shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+    {
+        variants: {
+            variant: {
+                default: "bg-primary text-primary-foreground hover:bg-primary/80",
+                outline:
+                    "border-border bg-background hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50",
+                secondary:
+                    "bg-secondary text-secondary-foreground hover:bg-[color-mix(in_oklch,var(--secondary),var(--foreground)_5%)] aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
+                ghost:
+                    "hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-muted/50",
+                destructive:
+                    "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
+                link: "text-primary underline-offset-4 hover:underline",
+            },
+            size: {
+                default:
+                    "h-8 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2",
+                xs: "h-6 gap-1 rounded-[min(var(--radius-md),10px)] px-2 text-xs in-data-[slot=button-group]:rounded-lg has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3",
+                sm: "h-7 gap-1 rounded-[min(var(--radius-md),12px)] px-2.5 text-[0.8rem] in-data-[slot=button-group]:rounded-lg has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3.5",
+                lg: "h-9 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2",
+                icon: "size-8",
+                "icon-xs":
+                    "size-6 rounded-[min(var(--radius-md),10px)] in-data-[slot=button-group]:rounded-lg [&_svg:not([class*='size-'])]:size-3",
+                "icon-sm":
+                    "size-7 rounded-[min(var(--radius-md),12px)] in-data-[slot=button-group]:rounded-lg",
+                "icon-lg": "size-9",
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+            size: "default",
+        },
+    }
+)
+function Button({
+    className,
+    variant = "default",
+    size = "default",
+    ...props
+}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+    return (
+        <ButtonPrimitive
+            data-slot="button"
+            className={cn(buttonVariants({ variant, size, className }))}
+            {...props}
+        />
+    )
+}
+
+// MODALES 
+function AlertDialog({ ...props }: AlertDialogPrimitive.Root.Props) {
+    return <AlertDialogPrimitive.Root data-slot="alert-dialog" {...props} />
+}
+function AlertDialogTrigger({ ...props }: AlertDialogPrimitive.Trigger.Props) {
+    return (
+        <AlertDialogPrimitive.Trigger data-slot="alert-dialog-trigger" {...props} />
+    )
+}
+function AlertDialogPortal({ ...props }: AlertDialogPrimitive.Portal.Props) {
+    return (
+        <AlertDialogPrimitive.Portal data-slot="alert-dialog-portal" {...props} />
+    )
+}
+function AlertDialogOverlay({
+    className,
+    ...props
+}: AlertDialogPrimitive.Backdrop.Props) {
+    return (
+        <AlertDialogPrimitive.Backdrop
+            data-slot="alert-dialog-overlay"
+            className={cn(
+                "fixed inset-0 isolate z-50 bg-black/10 duration-100 supports-backdrop-filter:backdrop-blur-xs data-open:animate-in data-open:fade-in-0 data-closed:animate-out data-closed:fade-out-0",
+                className
+            )}
+            {...props}
+        />
+    )
+}
+function AlertDialogContent({
+    className,
+    size = "default",
+    ...props
+}: AlertDialogPrimitive.Popup.Props & {
+    size?: "default" | "sm"
+}) {
+    return (
+        <AlertDialogPortal>
+            <AlertDialogOverlay />
+            <AlertDialogPrimitive.Popup
+                data-slot="alert-dialog-content"
+                data-size={size}
+                className={cn(
+                    "group/alert-dialog-content fixed top-1/2 left-1/2 z-50 grid w-full -translate-x-1/2 -translate-y-1/2 gap-4 rounded-xl bg-popover p-4 text-popover-foreground ring-1 ring-foreground/10 duration-100 outline-none data-[size=default]:max-w-xs data-[size=sm]:max-w-xs data-[size=default]:sm:max-w-sm data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95",
+                    className
+                )}
+                {...props}
+            />
+        </AlertDialogPortal>
+    )
+}
+function AlertDialogHeader({
+    className,
+    ...props
+}: React.ComponentProps<"div">) {
+    return (
+        <div
+            data-slot="alert-dialog-header"
+            className={cn(
+                "grid grid-rows-[auto_1fr] place-items-center gap-1.5 text-center has-data-[slot=alert-dialog-media]:grid-rows-[auto_auto_1fr] has-data-[slot=alert-dialog-media]:gap-x-4 sm:group-data-[size=default]/alert-dialog-content:place-items-start sm:group-data-[size=default]/alert-dialog-content:text-left sm:group-data-[size=default]/alert-dialog-content:has-data-[slot=alert-dialog-media]:grid-rows-[auto_1fr]",
+                className
+            )}
+            {...props}
+        />
+    )
+}
+function AlertDialogFooter({
+    className,
+    ...props
+}: React.ComponentProps<"div">) {
+    return (
+        <div
+            data-slot="alert-dialog-footer"
+            className={cn(
+                "-mx-4 -mb-4 flex flex-col-reverse gap-2 rounded-b-xl border-t bg-muted/50 p-4 group-data-[size=sm]/alert-dialog-content:grid group-data-[size=sm]/alert-dialog-content:grid-cols-2 sm:flex-row sm:justify-end",
+                className
+            )}
+            {...props}
+        />
+    )
+}
+function AlertDialogMedia({
+    className,
+    ...props
+}: React.ComponentProps<"div">) {
+    return (
+        <div
+            data-slot="alert-dialog-media"
+            className={cn(
+                "mb-2 inline-flex size-10 items-center justify-center rounded-md bg-muted sm:group-data-[size=default]/alert-dialog-content:row-span-2 *:[svg:not([class*='size-'])]:size-6",
+                className
+            )}
+            {...props}
+        />
+    )
+}
+function AlertDialogTitle({
+    className,
+    ...props
+}: React.ComponentProps<typeof AlertDialogPrimitive.Title>) {
+    return (
+        <AlertDialogPrimitive.Title
+            data-slot="alert-dialog-title"
+            className={cn(
+                "cn-font-heading text-base font-medium sm:group-data-[size=default]/alert-dialog-content:group-has-data-[slot=alert-dialog-media]/alert-dialog-content:col-start-2",
+                className
+            )}
+            {...props}
+        />
+    )
+}
+function AlertDialogDescription({
+    className,
+    ...props
+}: React.ComponentProps<typeof AlertDialogPrimitive.Description>) {
+    return (
+        <AlertDialogPrimitive.Description
+            data-slot="alert-dialog-description"
+            className={cn(
+                "text-sm text-balance text-muted-foreground md:text-pretty *:[a]:underline *:[a]:underline-offset-3 *:[a]:hover:text-foreground",
+                className
+            )}
+            {...props}
+        />
+    )
+}
+function AlertDialogAction({
+    className,
+    ...props
+}: React.ComponentProps<typeof Button>) {
+    return (
+        <Button
+            data-slot="alert-dialog-action"
+            className={cn(className)}
+            {...props}
+        />
+    )
+}
+function AlertDialogCancel({
+    className,
+    variant = "outline",
+    size = "default",
+    ...props
+}: AlertDialogPrimitive.Close.Props &
+    Pick<React.ComponentProps<typeof Button>, "variant" | "size">) {
+    return (
+        <AlertDialogPrimitive.Close
+            data-slot="alert-dialog-cancel"
+            className={cn(className)}
+            render={<Button variant={variant} size={size} />}
+            {...props}
+        />
+    )
+}
+
+
 
 export {
-    Avatar,
-    AvatarFallback,
-    Spinner,
-    Table,
-    TableHeader,
-    TableBody,
-    TableFooter,
-    TableHead,
-    TableRow,
-    TableCell,
-    TableCaption,
+    Avatar, AvatarFallback,
+    Spinner, Table,
+    TableHeader, TableBody,
+    TableFooter, TableHead,
+    TableRow, TableCell,
+    TableCaption, Button,
+    buttonVariants,
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogMedia,
+    AlertDialogOverlay,
+    AlertDialogPortal,
+    AlertDialogTitle,
+    AlertDialogTrigger,
 }
 
 
